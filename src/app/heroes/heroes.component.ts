ee2d2dafc1521f296a7a39b9002db947eecf5c1b
Nusrat <njahan8@centennialcollege.ca>
2020-01-06 23:15:34 -0500

    created delete and search heroes functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -29,4 +29,9 @@ export class HeroesComponent implements OnInit {
     if (!name) { return; }
     this.heroService.addHero({ name } as Hero).subscribe(hero => { this.heroes.push(hero); });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h != hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
